faecd64c85beb7a2b8d05f18b0bae4c0ccdaaf80
bemakonate <professorkonate@gmail.com>
2020-09-15 22:12:37 -0400

    Fix errors

--- src/components/layout/checkout/applepay.js
+++ src/components/layout/checkout/applepay.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { PaymentRequestButtonElement, useStripe } from '@stripe/react-stripe-js';
 import PaymentContext from './../../../context/apple-pay-context';
 import { connect } from 'react-redux';
+import * as layoutActions from '../../../store/layout/actions';
 
-const ApplePay = () => {
+const ApplePay = ({ ...props }) => {
 
     const paymentContext = useContext(PaymentContext);
     const [success, setSuccess] = useState(false);
@@ -40,43 +41,43 @@ const ApplePay = () => {
     }, [stripe]);
 
 
-    useEffect(() => {
-        paymentRequest.on('paymentmethod', async (ev) => {
-            // Confirm the PaymentIntent without handling potential next actions (yet).
-            const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
-                token,
-                { payment_method: ev.paymentMethod.id },
-                { handleActions: false }
-            );
+
+    paymentRequest.on('paymentmethod', async (ev) => {
+        // Confirm the PaymentIntent without handling potential next actions (yet).
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            token,
+            { payment_method: ev.paymentMethod.id },
+            { handleActions: false }
+        );
 
 
-            props.openFlashMessage('[applepay.js] paymentIntent', JSON.stringify(paymentIntent));
-            if (confirmError) {
-                // Report to the browser that the payment failed, prompting it to
-                // re-show the payment interface, or show an error message and close
-                // the payment interface.
-                ev.complete('fail');
-            } else {
-                // Report to the browser that the confirmation was successful, prompting
-                // it to close the browser payment method collection interface.
-                ev.complete('success');
-                // Check if the PaymentIntent requires any actions and if so let Stripe.js
-                // handle the flow. If using an API version older than "2019-02-11" instead
-                // instead check for: `paymentIntent.status === "requires_source_action"`.
-                if (paymentIntent.status === "requires_action") {
-                    // Let Stripe.js handle the rest of the payment flow.
-                    const { error } = await stripe.confirmCardPayment(clientSecret);
-                    if (error) {
-                        // The payment failed -- ask your customer for a new payment method.
-                    } else {
-                        // The payment has succeeded.
-                    }
+        props.openFlashMessage('[applepay.js] paymentIntent', JSON.stringify(paymentIntent));
+        if (confirmError) {
+            // Report to the browser that the payment failed, prompting it to
+            // re-show the payment interface, or show an error message and close
+            // the payment interface.
+            ev.complete('fail');
+        } else {
+            // Report to the browser that the confirmation was successful, prompting
+            // it to close the browser payment method collection interface.
+            ev.complete('success');
+            // Check if the PaymentIntent requires any actions and if so let Stripe.js
+            // handle the flow. If using an API version older than "2019-02-11" instead
+            // instead check for: `paymentIntent.status === "requires_source_action"`.
+            if (paymentIntent.status === "requires_action") {
+                // Let Stripe.js handle the rest of the payment flow.
+                const { error } = await stripe.confirmCardPayment(token);
+                if (error) {
+                    // The payment failed -- ask your customer for a new payment method.
                 } else {
                     // The payment has succeeded.
                 }
+            } else {
+                // The payment has succeeded.
             }
-        });
-    }, [])
+        }
+    });
+
     //=====================STRIPE DOCS===================
 
 
